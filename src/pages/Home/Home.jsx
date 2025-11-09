ae60b09450d8d34bbccb0df3ccf9c2adc4c0b962
rakibhassannayem <rakibhassannayem@gmail.com>
2025-11-09 19:46:23 +0600

    register ui

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,13 +3,30 @@ import Banner from "../../components/Banner/Banner";
 import Stats from "../../components/Stats/Stats";
 
 const Home = () => {
-  const data = useLoaderData();
-  const challengeImg = data.map((challenge) => challenge.imageUrl);
+  const challenges = useLoaderData();
+  const challengeImg = challenges.map((challenge) => challenge.imageUrl);
 
   return (
     <div className="space-y-10">
       <Banner challengeImg={challengeImg} />
       <Stats />
+
+      {/* Active Challenges */}
+      <div className="bg-base-300 py-10 px-5 rounded-xl">
+        <div className="flex justify-between mb-5">
+          <div className="text-center">
+            <h2 className="headings">Active Challenges</h2>
+            <p className="text-secondary mt-2">
+              Join ongoing sustainability challenges
+            </p>
+          </div>
+          <button className="btn btn-primary">View All</button>
+        </div>
+
+        {
+          
+        }
+      </div>
     </div>
   );
 };
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,85 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router";
 
 const Login = () => {
   return (
-    <div>
-      login
+    <div className="flex flex-col items-center mb-10">
+      <div className="flex flex-col items-center">
+        <img className="w-25" src="/logo.png" alt="" />
+        <h2 className="headings">Login to EcoTrack</h2>
+        <p className="text-secondary mt-2">
+          Welcome back! Continue your sustainability journey
+        </p>
+      </div>
+      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-lg mt-5 border p-4 pt-2">
+        <div className="mb-2">
+          <h3 className="headings text-xl!">Sign in</h3>
+          <p className="text-secondary text-sm">
+            Enter your credentials to access your account
+          </p>
+        </div>
+
+        <label className="label font-medium">Email</label>
+        <input
+          type="email"
+          name="email"
+          className="input w-full"
+          placeholder="Enter your email"
+        />
+
+        <label className="label font-medium">Password</label>
+        <input
+          type="password"
+          name="password"
+          className="input w-full"
+          placeholder="Enter your password"
+        />
+
+        <button className="btn btn-primary mt-4">Login</button>
+
+        <div className="divider divider-success text-secondary text-sm">
+          OR CONTINUE WITH
+        </div>
+
+        {/* Google */}
+        <button className="btn bg-white text-black border-[#e5e5e5]">
+          <svg
+            aria-label="Google logo"
+            width="16"
+            height="16"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+          >
+            <g>
+              <path d="m0 0H512V512H0" fill="#fff"></path>
+              <path
+                fill="#34a853"
+                d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+              ></path>
+              <path
+                fill="#4285f4"
+                d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+              ></path>
+              <path
+                fill="#fbbc02"
+                d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+              ></path>
+              <path
+                fill="#ea4335"
+                d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+              ></path>
+            </g>
+          </svg>
+          Login with Google
+        </button>
+        <p className="text-center text-secondary text-sm font-medium">
+          Don't have an account?
+          <Link to={"/register"} className="text-primary">
+            {" "}
+            Register here
+          </Link>
+        </p>
+      </fieldset>
     </div>
   );
 };
