e4a1675346990775322419ad9fcb12f695a0ce52
rakibhassannayem <rakibhassannayem@gmail.com>
2025-11-09 11:01:46 +0600

    router setup and layout

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
+import Home from "../pages/Home/Home";
+import Navbar from "../components/Navbar/Navbar";
+import { Outlet } from "react-router";
+import Footer from "../components/Footer/Footer";
 
 const MainLayout = () => {
   return (
-    <div>
-      
+    <div className="container mx-auto">
+      <Navbar />
+      <div className="my-4">
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   );
 };
